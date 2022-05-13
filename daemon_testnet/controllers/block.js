4a635629a960f5017b3e702a42a4498b1dc43f52
고유진 <zeke@goyujins-MacBook-Pro.local>
2022-05-13 10:03:03 +0900

    :memo: Add Block Response

--- daemon_testnet/controllers/block.js
+++ daemon_testnet/controllers/block.js
@@ -1,6 +1,16 @@
-const { Block } = require("../models");
-
+const models = require("../models");
 // 맞게 수정하기
 module.exports = {
-  getBlockInfo: async (req, res, next) => {},
+  getBlockInfo: async (req, res, next) => {
+    try {
+      const blockInfo = await models.block.findAll();
+      if (blockInfo) res.status(200).json({ blockInfo });
+    } catch (err) {
+      console.log(err);
+      res.status(404).send({
+        message: "server error",
+        errMsg: err,
+      });
+    }
+  },
 };
